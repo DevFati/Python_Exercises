22473c00ccc73dad5b1521e269d79ba06336e895
Fatima <fatmortcha@gmail.com>
2024-07-25 14:46:33 +0200

    subida ejer.

---
+++ Tipos_datos_simple/Ejercicio4.py
@@ -0,0 +1,9 @@
+#Escribir un programa que muestre por pantalla el resultado de la siguiente operación aritmética ((3+2)/2.5)^2
+
+import math
+
+# 1º forma
+print(math.pow(((3+2)/(2*5)),2))
+
+#2º forma
+print(((3+2)/(2*5))**2)
