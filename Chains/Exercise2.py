f0abdf2d48312b0ad41fa276fa5187a8bd906e97
Fatima <fatmortcha@gmail.com>
2024-07-30 23:55:22 +0200

    subida ej

---
+++ Chains/Exercise2.py
@@ -0,0 +1,10 @@
+#Write a program that asks the user's full name at the console and then displays the user's 
+# full name three times, once with all lowercase letters, once with all uppercase letters, 
+# and once with only the first letter of the name and surnames in capital letters. The user 
+# can enter their name combining upper and lower case as they wish.
+
+
+name =input("Give me your full name: ")
+
+print(name.lower)
+print(name.upper)
