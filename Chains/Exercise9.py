677068f17429768b15ab60f64ada1d727046627f
Fatima <fatmortcha@gmail.com>
2024-08-03 23:43:53 +0200

    subida ej

---
+++ Chains/Exercise9.py
@@ -0,0 +1,5 @@
+#Write a program that asks the user the date of birth in dd/mm/yyyy format and displays the day, 
+# month and year on the screen. Adapt the above program so that it also works when the day or 
+# month is entered with a single character.
+
+date=input("Introduce your date birth: ")
