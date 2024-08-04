2c35543371a64b9eb1aed476d6d70fd0d2d379d6
Fatima <fatmortcha@gmail.com>
2024-08-04 23:42:20 +0200

    mod ej

--- Chains/Exercise10.py
+++
@@ -1,9 +0,0 @@
-#Write a program that asks the console for the products in a shopping cart, separated by commas, 
-# and displays each of the products on the screen in a different line.
-
-products=input("Introduce the products separated by a comma: ")
-
-items=[str(i) for i in products.split(",")]
-
-for i in items:
-    print(i)
